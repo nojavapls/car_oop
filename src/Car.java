8063a8ab1b2fd7c89088c63a64a43d5336f7cd16
Millie <hey.kamikaze@yandex.ru>
2022-12-25 22:42:42 +0300

    update

--- src/Car.java
+++ src/Car.java
@@ -1,13 +1,34 @@
 class Car {
-    private String brand;
-    private String model;
+    final private String brand;
+    final private String model;
     private double engineVolume;
     private String color;
-    private int year;
-    private String country;
+    final private int year;
+    final private String country;
+    private String transmission;
+    final private String bodyType;
+    private String registrationNumber;
+    final private int numberOfSeats;
+    private boolean tires;
 
+    public class Key {
+        private boolean remoteEngineStart;
+        private boolean keylessAccess;
 
-    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        public Key(boolean remoteEngineStart, boolean keylessAccess){
+            if (remoteEngineStart != true || remoteEngineStart != false)
+                System.out.printf("Set correct remote Engine Start");
+            else
+                this.remoteEngineStart = remoteEngineStart;
+            if (keylessAccess != true || keylessAccess != false)
+                System.out.printf("Set correct keyless Access");
+            else
+                this.keylessAccess = keylessAccess;
+        }
+
+    }
+
+    public Car(String brand, String model, double engineVolume, String color, int year, String country, String transmission, String bodyType, String registrationNumber, int numberOfSeats, boolean tires) {
         if (engineVolume <= 0)
             this.engineVolume = 1.5;
         else
@@ -32,10 +53,89 @@ class Car {
             this.color = "белый";
         else
             this.color = color;
+        if (transmission == null || transmission.length() == 0)
+            this.transmission = "default";
+        else
+            this.transmission = transmission;
+        if (bodyType == null || bodyType.length() == 0)
+            this.bodyType = "default";
+        else
+            this.bodyType = bodyType;
+        if (registrationNumber == null || registrationNumber.length() == 0)
+            this.registrationNumber = "default";
+        else
+            this.registrationNumber = registrationNumber;
+        if (numberOfSeats == 0)
+            this.numberOfSeats = 0;
+        else
+            this.numberOfSeats = numberOfSeats;
+        if (tires != true || tires != false)
+            System.out.printf("Set correct tires");
+        else
+            this.tires = tires;
     }
 
+    public String getBrand(){
+        return this.brand;
+    }
+    public String getModel(){
+        return this.model;
+    }
+
+    public double getEngineVolume(){
+        return this.engineVolume;
+    }
+
+    public void setEngineVolume(double engineVolume){
+        this.engineVolume = engineVolume;
+    }
+    public String getColor(){
+        return this.color;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+    public int getYear(){
+        return this.year;
+    }
+    public String getCountry(){
+        return this.country;
+    }
+
+    public String getTransmission(){
+        return this.transmission;
+    }
+    public void setTransmission(String transmission){
+        this.transmission = transmission;
+    }
+    public String getBodyType(){
+        return this.bodyType;
+    }
+
+    public String getRegistrationNumber(){
+        return this.registrationNumber;
+    }
+    public void setRegistrationNumber(String registrationNumber){
+        this.registrationNumber = registrationNumber;
+    }
+    public int getNumberOfSeats(){
+        return this.numberOfSeats;
+    }
+
+    public boolean getTires(){
+        return this.tires;
+    }
+
+    public void changeTires(boolean tires){
+        this.tires = tires;
+    }
     public String toString() {
-        return ("Информация о машине: бренд " + this.brand + ", модель " + this.model + ", объем двигателя " + this.engineVolume + ", цвет  " + this.color+ ", год выпуска: " + this.year + ", страна производства: " + this.country);
+        String type;
+        if (this.tires == false)
+        type = "зимняя";
+                else
+        type = "летняя";
+        return ("Информация о машине: бренд " + this.brand + ", модель " + this.model + ", объем двигателя " + this.engineVolume + ", цвет  " + this.color+ ", год выпуска: " + this.year + ", страна производства: " + this.country + " коробка передач: " + this.transmission + "тип кузова: " + this.bodyType + "регистрационный номер: " + this.registrationNumber + "количество мест: " + this.numberOfSeats + "резина: " + type);
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car("Lada",  "Granta", 1.7, "желтый", 2015, "Россия");
-        Car car2 = new Car("Audi",  "A8 50 L TDI quattro", 3.0, null, 2020, "Германия");
-        Car car3 = new Car("BMW",  "Z8", 2021, null, 2015, "Германия");
-        Car car4 = new Car("Kia",  null, 2.4, "красный", 2018, "Южная Корея");
-        Car car5 = new Car("Hyundai",  "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+        Car car1 = new Car("Lada",  "Granta", 1.7, "желтый", 2015, "Россия", "автомат", "хэтчбек", "A666МР", 4, false);
+        Car car2 = new Car("Audi",  "A8 50 L TDI quattro", 3.0, null, 2020, "Германия", "автомат", "хэтчбек", "A126МР", 4, true);
+        Car car3 = new Car("BMW",  "Z8", 2021, null, 2015, "Германия", "автомат", "хэтчбек", "A001МР", 4, false);
+        Car car4 = new Car("Kia",  null, 2.4, "красный", 2018, "Южная Корея", "автомат", "хэтчбек", "A012МР", 4, true);
+        Car car5 = new Car("Hyundai",  "Avante", 1.6, "оранжевый", 2016, "Южная Корея", "автомат", "пикап", "A777МР", 4, false);
         System.out.println(car1.toString());
         System.out.println(car2.toString());
         System.out.println(car3.toString());
         System.out.println(car4.toString());
         System.out.println(car5.toString());
+        car5.changeTires(true);
+        System.out.println(car5.getTires());
     }
 }
